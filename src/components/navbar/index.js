bd25341d2f17c7b5c996e84105b5e432208075dc
Cristian <cristian.andrisen@gmail.com>
2022-11-02 15:24:23 -0300

    feat: ajustes de logo principal geraz

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { StarFilled } from "@ant-design/icons";
 import Button from "../button";
+import { StaticImage } from "gatsby-plugin-image";
 
 const itemFlex = {
   display: "flex",
@@ -14,45 +14,38 @@ const navbar = {
   height: "auto",
   minHeight: "4px",
   backgroundColor: "#151515",
-  borderBottom: "1px solid #000", 
-  paddingBottom: ".5rem"
+  borderBottom: "1px solid #000",
+  paddingBottom: ".5rem",
 };
 
 const navbarItens = {
   display: itemFlex.display,
-  marginBottom: ".5rem"
-  
+  marginBottom: ".5rem",
 };
 
 const navbarItem = {
   padding: "0.5rem 1rem 0.5rem 1rem ",
   fontWeight: "300",
-  fontSize: "16px !important"
+  fontSize: "16px !important",
 };
-
 
 const Navbar = () => {
   return (
-      <div style={navbar}>
-        <div className="logo">
-          <img
-            src="images/logoGeraZ.png"
-            loading="lazy"
-            width="48"
-            alt="logo "
-            class="main-logo"
-          />
-        </div>
-        <div className="nav" style={navbarItens}>
-          <div style={navbarItem}>Sobre nós</div>
-          <div style={navbarItem}>Nossos serviços</div>
-          <div style={navbarItem}>Clientes</div>
-          <div style={navbarItem}>Portfólio</div>
-          <div style={{marginLeft: "1rem"}}><Button text="ENTRE EM CONTATO" scale="0.9"/></div>
-        </div>
-        <div className="avaliable" style={{padding: "0.5rem 1rem"}}>
+    <div style={navbar}>
+      <div className="logo">
+      <StaticImage src="../../images/logo.png" alt="logo" width={48} />
+      </div>
+      <div className="nav" style={navbarItens}>
+        <div style={navbarItem}>Sobre nós</div>
+        <div style={navbarItem}>Nossos serviços</div>
+        <div style={navbarItem}>Clientes</div>
+        <div style={navbarItem}>Portfólio</div>
+        <div style={{ marginLeft: "1rem" }}>
+          <Button text="ENTRE EM CONTATO" scale="0.9" />
         </div>
       </div>
+      <div className="avaliable" style={{ padding: "0.5rem 1rem" }}></div>
+    </div>
   );
 };
 
